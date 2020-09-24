ee4d1c6fd3f0cbf34c48988ec4c72db454ab2c4f
Andres <softwareapgm@gmail.com>
2020-09-24 00:24:56 -0400

    declare main script area

--- startvm.sh
+++ startvm.sh
@@ -6,3 +6,14 @@ cBLUE='\033[0;34m'
 cRED='\033[0;31m'
 cYELLOW='\033[1;33m'
 cNONE='\033[0m'
+
+
+#START SCRIPT
+
+echo -e "${cYELLOW}--------------------${cGREEN}START${cYELLOW}-------------------------${cNONE}"
+echo -e "\t\t${cRED}KVM - Libvrt${cNONE}"
+
+
+
+echo -e "${cYELLOW}----------------------${cGREEN}DONE${cYELLOW}-------------------------${cNONE}"
+#END SCRIPT
