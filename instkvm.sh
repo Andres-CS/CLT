2253bf9bc92e90c023384152baee2c1329eabd4f
Andres <softwareapgm@gmail.com>
2021-04-06 19:27:00 -0400

    Updated script to check for CPU compatability

--- instkvm.sh
+++ instkvm.sh
@@ -18,6 +18,23 @@ function cpu64bit () {
     egrep -c ' lm ' /proc/cpuinfo 
 }
 
+#Check KVM is suppported
+function kvmok () {
+   echo "TEST START"
+   if [ `kvm-ok | egrep "command not found"` ] 
+   then
+	echo -e "Installing cpu-checker ${RED}TEMPORARLY${NC}."
+   	sudo apt-get install -y cpu-checker
+	#CALL KVMOK() RECURSIBLY
+	$kvmok
+   elif [ egrep -c "KVM acceleration can be used" kvm-ok ]
+   then
+	echo -e "${GREEN}GOOD${NC}"
+   fi
+   echo "TEST ENDS"
+
+}
+
 
 
 #Check for KVM Support
@@ -25,11 +42,9 @@ kvmspt=$( kvmcpusupport )
 #check return value 
 if [ $kvmspt -gt 0 ]
 then
-    echo -e "KVM CPU support: ${GREEN}YES${NC}"
-    echo -e "\tThis system has $kvmspt CPUs that support vitualization"
+    echo -e "KVM CPU support: ${GREEN}YES${NC}\tThis system has $kvmspt CPUs that support vitualization"
 else
-    echo -e "KVM CPU support: ${RED}NO${NC}"
-    echo -e "\tThis system has $kvmspt CPUs that support vitualization"
+    echo -e "KVM CPU support: ${RED}NO${NC}\tThis system has $kvmspt CPUs that support vitualization"
     #Exit script
     exit 1
 fi
@@ -38,11 +53,17 @@ fi
 lmspt=$( cpu64bit ) 
 if [ $lmspt -gt 0 ]
 then 
-    echo -e "64-bit Processor: ${GREEN}YES${NC}"
-    echo -e "\tThis system has a 64-bit Processor"
+    echo -e "64-bit Processor: ${GREEN}YES${NC}\tThis system has a 64-bit Processor"
 else
-    echo -e "64-bit Processor: ${RED}NO${NC}"
-    echo -e "\tThis system does not have 64-bit Processor"
+    echo -e "64-bit Processor: ${RED}NO${NC}\tThis system does not have 64-bit Processor"
     #Exit script
     exit 1
 fi
+
+#Check if KVM is supported on the system
+echo "T S"
+$kvmok
+echo `kvm-ok | egrep "command not found"`
+echo "T E"
+
+#sudo apt-get install -y qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manager
