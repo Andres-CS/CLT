b011bdfd5546ce88e247d19835e8e54077f0d861
Jarvis-Andy <softwareapgm@gmail.com>
2022-09-28 00:35:09 -0400

    testing read json file

--- powershell/goto.ps1
+++ powershell/goto.ps1
@@ -1,8 +1,15 @@
+# -------------------- 
+#   G VARIABLES 
+# -------------------- 
+
 $locationFolder = $env:HOMEPATH
 $folderName = ".gotoCommand"
 $fileName = "gotoConfig.json"
 $configFlag = 0 
 
+# -------------------- 
+#   FUNCTIONS Def
+# -------------------- 
 function Create-gotoInfo {
     param(
         $location,
@@ -10,42 +17,42 @@ function Create-gotoInfo {
         $fileName
         )
     
-    if ($type -match "d"){
-        Write-Host "D option"
-        New-Item -ItemType "Directory" -Path $location -Name $fileName
-    }
+    if ($type -match "d"){ New-Item -ItemType "Directory" -Path $location -Name $fileName }
     
-    if ($type -match "f"){
-        Write-Host "F option"
-        New-Item -ItemType "file" -Path $location -Name $fileName
-    }
+    if ($type -match "f"){ New-Item -ItemType "file" -Path $location -Name $fileName }
 }
 
-#Check goto folder exisits
+function readFile {
+
+}
+
+
+# -------------------- 
+#   START SCRIPT 
+# -------------------- 
+
+#Check "goto" folder exisits
 if ( Test-Path -Path ($locationFolder+"\"+$folderName)){
-    #Check config file exisists
+    #Check "config" file exisists
     if(Test-Path -Path ($locationFolder+"\"+$folderName+"\"+$fileName)){  }
-    else{ 
-        $configFlag = 1
-    }
+    else{  $configFlag = 1  }
 }
-else{ 
-    $configFlag = 2
-}
-
-
-
+else{  $configFlag = 2  }
 
 switch($configFlag){
     1 { 
-        Write-Host " ** FILE CREATED"
+        Write-Host " ** FILE CREATED ** "
         Create-gotoInfo -type "f" -location ($locationFolder+"\"+$folderName) -fileName $fileName
     }
     
     2 {
-        Write-Host " ** FOLDER AND FILE CREATED"
+        Write-Host " ** FOLDER AND FILE CREATED ** "
         Create-gotoInfo -type "d" -location $locationFolder -fileName $folderName
         Create-gotoInfo -type "f" -location ($locationFolder+"\"+$folderName) -fileName $fileName
     }
 }
 
+#Read config file
+
+$test = Get-Content ($locationFolder+"\"+$folderName+"\"+$fileName) | ConvertFrom-Json
+$test.njit.root
