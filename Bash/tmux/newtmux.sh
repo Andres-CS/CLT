879b1d398abc76f734ad82e9a1a1d5bfa6398820
Andy <softwareapgm@gmail.com>
2022-10-06 04:10:38 -0400

    testing color in pane

--- Bash/tmux/newtmux.sh
+++ Bash/tmux/newtmux.sh
@@ -1,17 +1,20 @@
-#tmux new -s ANDRES -d
-#tmux split-window -t ANDRES:0 -h
-#tmux new-window -t ANDRES:1
-#tmux split-window -t ANDRES:0.0 -v
-#tmux send-keys -t ANDRES:0.0 "nano newtmux.sh" Enter
-#tmux send-keys -t ANDRES:0.2 htop Enter
-#tmux split-window -t ANDRES:0.2 -h
-#tmux send-key -t ANDRES:0.3 "ping 8.8.8.8" Enter
+#tmux new -s CS644 -d
+#tmux split-window -t CS644:0 -h
+#tmux new-window -t CS644:1
+#tmux split-window -t CS644:0.0 -v
+#tmux send-keys -t CS644:0.0 "nano newtmux.sh" Enter
+#tmux send-keys -t CS644:0.2 htop Enter
+#tmux split-window -t CS644:0.2 -h
+#tmux send-key -t CS644:0.3 "ping 8.8.8.8" Enter
 
-tmux new -s ANDRES -d
-tmux split-window -t ANDRES:0 -v
-tmux select-pane -t ANDRES:0.0
-tmux split-pane -t ANDRES:0.0 -h
-tmux select-pane -t ANDRE:0.1
-tmux split-window -t ANDRES:0.1 -h
-tmux select-pane -t ANDRES:0.0
-tmux split-window -t ANDRES:0.0 -h
+tmux new -s CS644 -d
+tmux split-window -t CS644:0 -v
+tmux select-pane -t CS644:0.0
+tmux split-pane -t CS644:0.0 -h
+tmux select-pane -t CS644:0.1
+tmux split-window -t CS644:0.1 -h
+tmux select-pane -t CS644:0.0
+tmux split-window -t CS644:0.0 -h
+tmux select-pane -t CS644:0.4 -P 'bg=yellow' -T "gatoGrodo"
+tmux resize-pane -U 30
+tmux set-option pane-border-status bottom
