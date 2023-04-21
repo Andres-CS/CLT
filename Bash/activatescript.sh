7cdd6cc28b51fc2b00559995f0716b0569c7ff90
Jairo <jairo.perez@noaa.gov>
2023-04-21 13:40:04 -0400

    change cp for symlink & added figlet for ascii art

--- Bash/activatescript.sh
+++ Bash/activatescript.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
 #Checked default location is there otherwise create it.
-location="/home/$USER/.local/bin/"
+default_location="/home/$USER/.local/bin/"
 
-if [ ! -d $location ]
+if [ ! -d $default_location ]
 then
-    mkdir -p $location
+    mkdir -p $default_location
 fi
 
+figlet "Activate Script"
+echo -e "\nPlease enter FULL path of script."
+read -p "Path: " script_path
 
-read -p "Script: " script_path
+echo -e "\nPlease enter alias name for script, if desired."
+read -p "Alias [enter if none]: " alias
 
 #Checked file exits
 
 if [ -f $script_path ]
 then
-    cp $script_path $location
+    if [ $alias == "" ]
+    then 
+        ln -s $script_path $default_location
+    else
+        ln -s $script_path $default_location"/"$alias
+    fi
+
 else
     echo "File not found or path is wrong"
 fi
 
 #List files in location
 
-ls -l $location
+ls -l $default_location
 
 
 
